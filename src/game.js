b1855909167c2cf26db52e540f542e835f3321b2
Jethet <henriette.hettinga@gmail.com>
2020-01-23 16:52:07 +0100

    Add functionality for collision and counting player's lives

--- src/game.js
+++ src/game.js
@@ -1,110 +1,127 @@
-'use strict'
+"use strict";
 
-// Create game constructor
+function Game() {
+  this.canvas = null;
+  this.ctx = null;
 
-function Game(){
-    this.canvas = null;
-    this.ctx =  null;
+  this.enemies = []; // push enemiy objects at random
+  this.player = null;
 
-    this.enemies = [];   // array of enemies
-    this.player = null;
+  this.gameIsOver = false;
+  this.gameScreen = null;
+}
 
-    this.gameIsOver = false;
-    this.gameScreen = null;   // to save the game
+// Initialize the game and canvas
+Game.prototype.start = function() {
+  this.canvasContainer = document.querySelector(".canvas-container");
+  this.canvas = this.canvasContainer.querySelector("canvas");
+  this.ctx = this.canvas.getContext("2d");
 
+  this.livesElement = this.gameScreen.querySelector(".lives .value");
+  this.scoreElement = this.gameScreen.querySelector(".score .value");
+
+  var containerWidth = this.canvasContainer.offsetWidth;
+  var containerHeight = this.canvasContainer.offsetHeight;
+
+  this.canvas.setAttribute("width", containerWidth);
+  this.canvas.setAttribute("height", containerHeight);
+
+  // Create the player
+  this.player = new Player(this.canvas, 3, 100);
+
+  // Add keydown event listeners
+  this.handleKeyDown = function(event) {
+    if (event.key === "ArrowUp") {
+      console.log("UP");
+      this.player.setDirection("up");
+    } else if (event.key === "ArrowDown") {
+      console.log("DOWN");
+      this.player.setDirection("down");
+    }
+  };
+
+  // this = game instance
+  window.addEventListener("keydown", this.handleKeyDown.bind(this));
+
+  // Start the game initially
+  this.startLoop();
 };
 
-Game.prototype.start = function(){   // initialize game and canvas: grab elements to work with (parent element has size and
-    this.canvasContainer = document.querySelector('.canvas-container'); // size of the canvas must be within parent of canvas
-    this.canvas = this.canvasContainer.querySelector('canvas');
-    this.ctx = this.canvas.getContext('2d');
+Game.prototype.startLoop = function() {
+  var loop = function() {
+    console.log("IN LOOP");
 
-    this.livesElement = this.gameScreen.querySelector('.lives .value');
-    this.scoreElement = this.gameScreen.querySelector('.score .value');
+    // 1. UPDATE THE STATE (game, player, enemy)
 
-    var containerWidth = this.canvasContainer.offsetWidth;    // built-in property to take entire size
-    var containerHeight = this.canvasContainer.offsetHeight;   // of the element, incl borders etc. ??
+    // 0. Player was created already
 
-    this.canvas.setAttribute('width', containerWidth);
-    this.canvas.setAttribute('height', containerHeight);
+    // 1. Create enemies randomly
 
-    // Create player
-    this.player = new Player(this.canvas, 3, 100);  // is created by player constructor
+    if (Math.random() > 0.98) {
+      var randomY = this.canvas.height * Math.random();
+      var newEnemy = new Enemy(this.canvas, randomY, 5);
 
-    // Add keydown event listeners (for movements)
-    this.handleKeyDown = function(event){
-        if (event.key === 'ArrowUp'){
-            console.log("UP");
-            this.player.setDirection('up');
-        } else if (event.key === 'ArrowDown'){
-            console.log("DOWN");
-            this.player.setDirection('down');
-        }
-    };
+      this.enemies.push(newEnemy);
+    }
 
-    window.addEventListener('keydown', this.handleKeyDown.bind(this)); // this.handleKeyDown is callback, need to bind to object player
-// BIND is necessary when using window because by default the event listener is applied to
-// window. If it is meant to aplly to something else, bind() is needed. 
-// Window is needed here because the event listener needs to get the events from the entire
-// window, wherever the event takes place.
+    // 2. Check if the player had collisions with enemies (check all of the enemies)
+    this.checkCollisions();
 
-    // Start the game initially
-    this.startLoop();
+    // 3. Update the player and check if he is colliding the screen
+    this.player.handleScreenCollision();
 
+    // 4. Update the existing enemies (move them)
+    // 5. Check if the enemies our out of the screen
+    // [x, x, x ,x ]
+
+    this.enemies = this.enemies.filter(function(enemyObj) {
+      enemyObj.updatePosition(); // 4
+      return enemyObj.isInsideScreen(); // 5
+    });
+
+    // 2. CLEAR THE CANVAS
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // 3. UPDATE THE CANVAS (DRAW)
+    // 1. Draw the player
+    this.player.draw();
+
+    // 2. Draw all of the enemies
+    this.enemies.forEach(function(enemyObj) {
+      enemyObj.draw();
+    });
+
+    // 4. TERMINATE THE LOOP IF THE GAME IS OVER
+    if (!this.gameIsOver) {
+      requestAnimationFrame(loop);
+    }
+  }.bind(this);
+
+  // requestAnimationFrame(loop);
+  loop();
 };
 
-Game.prototype.startLoop = function(){
-    var loop = function(){
-        console.log('loop');
+Game.prototype.updateGameStats = function() {};
 
-    // 1. update state of player, game, enemy: 
-        // a. player was created already; 
-        // b. enemies must be created randomly;
-        if (Math.random() > 0.98){
-            var randomY = this.canvas.height * Math.random();
-            var newEnemy = new Enemy(this.canvas, randomY, 5);
-            this.enemies.push(newEnemy);
-        }
-        // c. collisions of player need to be checked (check all of the enemies);
-        this.checkCollisions();
-
-        // d. update the player and check if he is colliding with the screen
-        this.player.handleScreenCollision();
-
-        // e. update the existing enemies (move them)
-        // f. check if enemies are out of the screen (i.e. create array with enemies
-        // who pass these conditions)
-        this.enemies = this.enemies.filter(function(enemyObj){
-            enemyObj.updatePosition();
-            return enemyObj.isInsideScreen();  // will evaluate to true or false
-        });
-
-    // 2. clear the canvas
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    // 3. update the canvas (draw)
-    //  a. draw the player
-        this.player.draw();
-
-    //  b. draw the enemies
-        this.enemies.forEach(function(enemyObj){
-            enemyObj.draw()
-    })
-
-    // 4. terminate the loop if game is over
-
-    if (!this.gameIsOver){
-        requestAnimationFrame(loop);
-    };
-    }.bind(this);
-    loop();
+Game.prototype.gameOver = function() {
+    this.gameIsOver = true;
+    console.log("GAME OVER");
 };
 
+Game.prototype.removeGameScreen = function() {};
 
-Game.prototype.updateGameStats = function(){};
+Game.prototype.checkCollisions = function() {
+  this.enemies.forEach(function(enemy) {
+    if (this.player.didCollide(enemy)) {
+      this.player.removeLife();
+      console.log("lives", this.player.lives);
 
-Game.prototype.gameOver = function(){};
+      // move the enemy out of the screen
+      enemy.x = 0 - enemy.size;
 
-Game.prototype.removeGameScreen = function(){};
-
-Game.prototype.checkCollisions = function(){};
+      if (this.player.lives === 0) {
+        this.gameOver();
+      }
+    }
+  }, this);
+};
--- src/player.js
+++ src/player.js
@@ -1,54 +1,73 @@
-'use strict'
+"use strict";
 
-// Constructor for player
+function Player(canvas, lives, size) {
+  this.canvas = canvas;
+  this.ctx = canvas.getContext("2d");
 
-function Player(canvas, lives){
-    this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
-    this.lives = lives;
-    this.size = 100;
+  this.lives = lives;
+  this.size = size;
 
-    this.x = 50;
-    this.y = canvas.height / 2; // this is the start position: centered
+  this.x = 50;
+  this.y = canvas.height / 2;
 
-    this.direction = 0;     // direction is only up and down so can be 1, 0 or -1
-    this.speed = 5;
+  this.direction = 0;
+  this.speed = 5;
 }
 
-Player.prototype.setDirection = function(direction){
-    // increase with -1 for up, +1 for down due to y axis
-    if (direction === 'up') this.direction = -1;
-    else if (direction === 'down') this.direction = 1;
-    else if (direction === 'stop') this.direction = 0;
+Player.prototype.setDirection = function(direction) {
+  // +1 down  -1 up
+  if (direction === "up") this.direction = -1;
+  else if (direction === "down") this.direction = 1;
+  else if (direction === "stop") this.direction = 0;
 };
 
-Player.prototype.didCollide = function(){  
+Player.prototype.didCollide = function(enemy) {
+  var playerLeft = this.x;
+  var playerRight = this.x + this.size;
+  var playerTop = this.y;
+  var playerBottom = this.y + this.size;
 
+  var enemyLeft = enemy.x;
+  var enemyRight = enemy.x + enemy.size;
+  var enemyTop = enemy.y;
+  var enemyBottom = enemy.y + enemy.size;
 
+  var crossRight = enemyLeft <= playerRight && enemyRight >= playerLeft;
+
+  var crossLeft = enemyRight >= playerLeft && enemyLeft <= playerRight;
+
+  var crossTop = enemyBottom >= playerTop && enemyTop <= playerBottom;
+
+  var crossBottom = enemyBottom <= playerBottom && enemyBottom >= playerTop;
+
+  if ((crossLeft || crossRight) && (crossTop || crossBottom)) {
+    return true;
+  }
+
+  return false;
 };
 
-Player.prototype.updatePosition = function(){
-    this.y = this.y + (this.direction * this.speed);
-    
+Player.prototype.updatePosition = function() {
+  this.y = this.y + this.direction * this.speed; //  + 5   - 5
 };
 
-Player.prototype.handleScreenCollision = function(){
-    this.updatePosition();
-    var screenTop = 0;                      
-    var screenBottom = this.canvas.height;
+Player.prototype.handleScreenCollision = function() {
+  this.updatePosition();
 
-    if (this.y + this.size > screenBottom) this.direction = -1;
-    else if (this.y < screenTop) this.direction = 1;
+  var screenTop = 0;
+  var screenBottom = this.canvas.height;
+
+  if (this.y + this.size > screenBottom) this.direction = -1;
+  else if (this.y < screenTop) this.direction = 1;
 };
 
+Player.prototype.removeLife = function() {
+    this.lives -= 1;
+};
 
+Player.prototype.draw = function() {
+  this.ctx.fillStyle = "lightblue";
 
-Player.prototype.removeLife = function(){};
-
-Player.prototype.draw = function(){
-    this.ctx.fillStyle = 'green';
-
-    // fillRect(x, y, width, heitght):
-    this.ctx.fillRect(this.x, this.y, this.size, this.size) // using this.size 2 times gives a square size
-
+  // fillRect(x, y, width, height)
+  this.ctx.fillRect(this.x, this.y, this.size, this.size);
 };
